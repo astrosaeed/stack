b009e2cc7dc39fa5ce593203782cddaacee50a03
Saeid <astrosaeed@gmail.com>
2021-04-09 13:20:17 -0400

    wrote the collection loop

--- data_collection.py
+++ data_collection.py
@@ -11,7 +11,8 @@ import shutil
 from datetime import datetime
 
 
-
+obj_geometry = {'Cuboid':(0.01,0.01,0.01), 'Cuboid0':(0.01,0.02,0.01),'Cuboid1':(0.02,0.01,0.01)\
+				,'Cuboid2':(0.02,0.01,0.01),'Cuboid3':(0.01,0.01,0.01),'Cuboid4':(0.01,0.01,0.01) }
 is_sim =True
 workspace_limits = np.asarray([[-0.724, -0.276], [-0.224, 0.224], [-0.0001, 0.4]]) # Cols: min max, Rows: x y z (define workspace limits in robot coordinates)
 robot = Robot(is_sim, workspace_limits)
@@ -21,10 +22,12 @@ class StackingScene:
 	def __init__(self,num_objects):
 		robot.restart_sim()
 		self.num_objects = num_objects
+
+		self.initialize_objects(num_objects)
+		#self.run_trial_stability()		
+		
+	def initialize_objects(self,num_objects):
 		self.object_list = ['Cuboid']+['Cuboid'+str(i) for i in range(num_objects-1)]
-		self.run_trial_stability()		
-		
-		
 
 	def get_object_position(self, object_name):
 		sim_ret, handle  =vrep.simxGetObjectHandle(robot.sim_client,object_name,vrep.simx_opmode_blocking)
@@ -61,22 +64,43 @@ class StackingScene:
 			#if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 		######## Adding a test of the image creation
+	def put_aside(self):
+		for i,obj in enumerate(obj_geometry.keys()):
+			#print (top_object_z)
+			#self.set_object_position(obj,0,0,0.01*(i+1))
+			self.set_object_position(obj,0.1,0.1,0)
+		
 
 	def set_the_scene(self):
 		# Select the random location of the first item
 		print (self.object_list)
+		
+#		time.sleep(2)
 		first_object = random.choice(self.object_list)
 		x,y,z = self.get_object_position(first_object)
-		self.set_object_position(first_object,0,0,0.01)
+		self.set_object_position(first_object,0,0,0+obj_geometry[first_object][2]/2)
 		self.object_list.remove(first_object)
 
 		#print (temp)
+		top_object = first_object
+		top_object_z = self.get_object_position(top_object)[2]
+
+		random.shuffle(self.object_list) 
 		for i,obj in enumerate(self.object_list):
-			self.set_object_position(obj,0,0,0.01*(i+1))
+			print (top_object_z)
+			#self.set_object_position(obj,0,0,0.01*(i+1))
+			self.set_object_position(obj,0,0,obj_geometry[obj][2]/2+top_object_z+obj_geometry[top_object][2]/2)
+			x,y,z = self.get_object_position(obj)
+			#print (obj_geometry[top_object][2])
+			#input()
+			top_object = obj
+			top_object_z = self.get_object_position(top_object)[2]
+			
 		print ('Done')
 
 	def run_trial_stability(self):
 		vrep.simxStopSimulation(robot.sim_client, vrep.simx_opmode_blocking)
+		self.put_aside()
 		self.set_the_scene()
 		positions_now = np.array([self.get_object_position(obj) for obj in self.object_list])
 		
@@ -92,10 +116,16 @@ class StackingScene:
 		os.rename('new2.jpg',date_time+'.jpg')
 		if np.linalg.norm((positions_now - positions_next), ord=1)>0.01:
 			print ('unstable')
-			shutil.move(date_time+'.jpg','./data/unstable')
+			shutil.move(date_time+'.jpg','/home/saeid/data/unstable')
 		else:
 			print ('stable')
-			shutil.move(date_time+'.jpg','./data/stable')
+			shutil.move(date_time+'.jpg','/home/saeid/data/stable')
+
+			##### IMPORTANT #######
+			### ADD OBJECT info to some data structure
+			### Make sure to add noise to locations
+			### ADD More Sensors
+
 		# else return stable
 
 
@@ -109,7 +139,12 @@ class StackingScene:
 
 def main():
 
-	obj = StackingScene(4)
+	for i in range(2,len(obj_geometry.keys())):
+
+		obj = StackingScene(i)
+		for j in range(10):
+			obj.run_trial_stability()
+			obj.initialize_objects(i)
 
 
 
